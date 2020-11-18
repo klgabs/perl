6d320d0aeb2682d88165699750c9c6b2cd4c1d79
klgabs <kayefyne@hotmail.com>
2020-11-18 09:35:54 +0800

    Update zip_and_move.pl

--- zip_and_move.pl
+++ zip_and_move.pl
@@ -1,6 +1,5 @@
 #! /usr/bin/env perl
 
-package Main;
 use feature qw(say);
 use strict;
 use warnings;
@@ -8,27 +7,42 @@ use Data::Dumper;
 use File::Basename qw(basename);
 use File::Spec;
 {
-    my $self = Main->new(
-        sourcedir => '/home/project/work/karen/data/env/hold',
-        destdir   => '/home/project/work/karen/data/env/PROD',
-        maxfiles  => 1000,
-        maxsize   => 10_000_000,         # 1 Mb = 1_000_000 bytes
-    );
-   
-    $self->init();
-    $self->get_files();
-    $self->start_new_archive();
-    while ($self->{cur_files_left} > 0) {
-        $self->add_file();
-        if ($self->{cur_num_arch_files} >= $self->{maxfiles}) {
-            $self->start_new_archive();
+    my @sourcedirs = ('/archives/data/mefab/STAGING/PRODUCTION', '/archives/data/bkfb/STAGING/PRODUCTION');
+    my @destdirs   = ('/archives/data/mefab/PRODUCTION', '/archives/data/bkfb/PRODUCTION');
+    my $maxfiles  = 1000;
+    my $maxsize = 10000000;
+    for my $idx (0..$#sourcedirs) {
+        my $src = $sourcedirs[$idx];
+        my $dst = $destdirs[$idx];
+        say "\nWorking on sourcedir: $src, destdir: $dst ..\n";
+        my $zip = ZipDir->new(
+            sourcedir => $src,
+            destdir   => $dst,
+            maxfiles  => $maxfiles,
+            maxsize   => $maxsize,
+        );
+        $zip->init();
+        $zip->get_files();
+        $zip->start_new_archive();
+        while ($zip->{cur_files_left} > 0) {
+            $zip->add_file();
+            if ($zip->{cur_num_arch_files} >= $zip->{maxfiles}) {
+                $zip->start_new_archive();
+            }
+            elsif ($zip->cur_arch_size() >= $zip->{maxsize}) {
+                $zip->start_new_archive();
+            }
         }
-        elsif ($self->cur_arch_size() >= $self->{maxsize}) {
-            $self->start_new_archive();
-        }
+        $zip->cleanup();
     }
     say "Done.";
 }
+
+package ZipDir;
+use Cwd qw(getcwd);
+use Data::Dumper;
+use File::Basename qw(basename dirname);
+use File::Spec;
 
 sub add_file {
     my ( $self ) = @_;
@@ -38,7 +52,12 @@ sub add_file {
     die "Unexpected, file does not exist" if !(-e $fn && -f $fn);
     $self->{sum_file_sizes} += -s $fn;
     say ".. $fn";
-    system "zip", "-q", $self->{zip_fn}, $fn;
+    my $res = system "zip", "-q", $self->{zip_fn}, $fn;
+    if ( $res != 0 ) {
+        my $ret_val = $res >> 8;
+        die "Failed to execute zip command. zip returned exit code $ret_val";
+    }
+    unlink $fn or die "Could not delete file '$fn': $!";
     $self->{cur_files_left} = scalar @$files;
     $self->{cur_num_arch_files} += 1;
 }
@@ -47,6 +66,12 @@ sub cd {
     my ( $self,  $dir ) = @_;
 
     chdir $dir or die "Could not chdir to '$dir': $!";
+}
+
+sub cleanup {
+    my ( $self ) = @_;
+
+    $self->cd( $self->{cwd} );
 }
 
 sub cur_arch_size {
@@ -69,9 +94,11 @@ sub get_files {
 sub init {
     my ( $self ) = @_;
 
+    $self->{cwd} = getcwd();
     $self->{zip_count} = 0;
     $self->{cur_num_arch_files} = 0;
     $self->{sourcedir} = File::Spec->rel2abs($self->{sourcedir});
+    $self->{parent_dir_name} = basename(dirname( $self->{sourcedir} ));
     $self->{destdir} = File::Spec->rel2abs($self->{destdir});
     $self->{zip_fn} = undef;
     $self->{sum_file_sizes} = 0;
@@ -99,7 +126,8 @@ sub start_new_archive {
     chomp $date;
     $self->{zip_count} += 1;
     my $count = sprintf "%04d", $self->{zip_count};
-    my $fn = $count . "_" . $date . ".zip";
+    #my $fn = $count . "_" . $date . ".zip";
+    my $fn = $self->{parent_dir_name} . "_" . $date . "_" . $count . ".zip";
     $self->{zip_fn} = File::Spec->catfile($self->{destdir}, $fn);
     $self->{cur_num_arch_files} = 0;
 }
